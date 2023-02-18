0d5f451eac88e81cb684a8d94d5da925ff026496
zoelabbb <zoelabbb@gmail.com>
2023-02-18 20:53:30 +0700

    New File

--- public/index.php
+++ public/index.php
@@ -627,7 +627,7 @@ Crafts
 
 <div class="block2">
 <div class="block2-pic hov-img0">
-<img src="./images/product-01.jpg" alt="IMG-PRODUCT">
+<img src="<?php echo base_url(); ?>./images/product-01.jpg" alt="IMG-PRODUCT">
 <a href="index.html#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
 Quick View
 </a>
