b429aee151dc33bd0eb7df11671a12d26fb78343
zoelabbb <zoelabbb@gmail.com>
2023-02-18 20:06:18 +0700

    Settings DB

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
+        'username' => 'root',
         'password' => '',
-        'database' => '',
+        'database' => 'db_carkecor',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
