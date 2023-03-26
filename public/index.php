4d7951635a263048e0154bc95a853fe2bae4e411
zoelabbb <zoelabbb@gmail.com>
2023-03-27 00:54:19 +0700

    Update App.php & Index.php

--- public/index.php
+++ public/index.php
@@ -55,30 +55,7 @@
 	<!-- WRAPPER START -->
 	<div class="wrapper">
 
-		<!-- Mobile-header-top Start -->
-		<div class="mobile-header-top d-block d-md-none">
-			<div class="container">
-				<div class="row">
-					<div class="col-12">
-						<!-- header-search-mobile start -->
-						<div class="header-search-mobile">
-							<div class="table">
-								<div class="table-cell">
-									<ul>
-										<li><a class="search-open" href="index.html#"><i class="zmdi zmdi-search"></i></a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/login.html"><i class="zmdi zmdi-lock"></i></a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/my-account.html"><i class="zmdi zmdi-account"></i></a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/wishlist.html"><i class="zmdi zmdi-favorite"></i></a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<!-- header-search-mobile start -->
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- Mobile-header-top End -->
+
 		<!-- HEADER-AREA START -->
 		<header id="sticky-menu" class="header">
 			<div class="header-area">
@@ -106,111 +83,7 @@
 			<div class="main-menu  d-none d-md-block">
 				<nav>
 					<ul>
-						<li><a href="index.html">home</a>
-							<div class="sub-menu menu-scroll">
-								<ul>
-									<li class="menu-title">Page's</li>
-									<li><a href="index.html">Home Version 1</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/index-2.html">Home Version 2</a></li>
-								</ul>
-							</div>
-						</li>
-						<li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop.html">products</a>
-							<div class="mega-menu menu-scroll">
-								<div class="table">
-									<div class="table-cell">
-										<div class="half-width">
-											<ul>
-												<li class="menu-title">best brands</li>
-												<li><a href="index.html#">henning koppel</a></li>
-												<li><a href="index.html#">jehs + Laub</a></li>
-												<li><a href="index.html#">vicke lindstrand</a></li>
-												<li><a href="index.html#">don chadwick</a></li>
-												<li><a href="index.html#">akiko kuwahata</a></li>
-												<li><a href="index.html#">barbro berlin</a></li>
-												<li><a href="index.html#">cecilia hall</a></li>
-												<li><a href="index.html#">don chadwick</a></li>
-											</ul>
-										</div>
-										<div class="half-width">
-											<ul>
-												<li class="menu-title">popular brands</li>
-												<li><a href="index.html#">akiko kuwahata</a></li>
-												<li><a href="index.html#">barbro berlin</a></li>
-												<li><a href="index.html#">cecilia hall</a></li>
-												<li><a href="index.html#">don chadwick</a></li>
-												<li><a href="index.html#">henning koppel</a></li>
-												<li><a href="index.html#">jehs + Laub</a></li>
-												<li><a href="index.html#">vicke lindstrand</a></li>
-												<li><a href="index.html#">don chadwick</a></li>
-											</ul>
-										</div>
-										<div class="full-width">
-											<div class="mega-menu-img">
-												<a href="https://template.hasthemes.com/hurst-v1/hurst/single-product.html"><img src="img/megamenu/1.jpg" alt="" /></a>
-											</div>
-										</div>
-										<div class="pb-80"></div>
-									</div>
-								</div>
-							</div>
-						</li>
-						<li><a href="index.html#">Shortcodes</a>
-							<div class="sub-menu menu-scroll">
-								<ul>
-									<li class="menu-title">Shortcodes</li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-accordions.html">Accordion</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-toggles.html">Toggles</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-tab.html">Tab</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-product-tab.html">Product Tab</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-product-tab-2.html">Product Tab 2</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-carousel.html">product carousel</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-carousel-2.html">product carousel 2</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-featured-product.html">Featured Product</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-featured-product-2.html">Featured Product 2</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-button.html">Button</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-table.html">Table</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-progress-bars.html">Progress Bar</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-blog.html">Blog</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-blog-2.html">Blog - 2</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-team.html">Team</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-footer.html">Footer</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-footer-2.html">Footer 2</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/elements-map.html">Map</a></li>
-								</ul>
-							</div>
-						</li>
-						<!-- <li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop-sidebar.html">accesories</a></li> -->
-						<!-- <li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop-list.html">lookbook</a></li> -->
-						<!-- <li><a href="https://template.hasthemes.com/hurst-v1/hurst/blog.html">blog</a></li> -->
-						<!-- <li><a href="index.html#">pages</a>
-								<div class="sub-menu menu-scroll">
-									<ul>
-										<li class="menu-title">Page's</li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop.html">Shop</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop-sidebar.html">Shop Sidebar</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop-grid-right-sidebar.html">Shop Right Sidebar</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop-list.html">Shop List</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop-list-right-sidebar.html">Shop List right sidebar</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/single-product.html">Single Product</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/single-product-sidebar.html">Single Product Sidebar</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/cart.html">Shopping Cart</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/wishlist.html">Wishlist</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/checkout.html">Checkout</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/order.html">Order</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/login.html">login / Registration</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/my-account.html">My Account</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/404.html">404</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/blog.html">Blog</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/single-blog.html">Single Blog</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/single-blog-sidebar.html">Single Blog Sidebar</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/about.html">About Us</a></li>
-										<li><a href="https://template.hasthemes.com/hurst-v1/hurst/contact.html">Contact</a></li>
-									</ul>
-								</div>
-							</li> -->
-						<li><a href="https://template.hasthemes.com/hurst-v1/hurst/about.html">about us</a></li>
-						<li><a href="https://template.hasthemes.com/hurst-v1/hurst/contact.html">contact</a></li>
+						<li><a href="index.html">Home</a></li>
 					</ul>
 				</nav>
 			</div>
@@ -226,38 +99,7 @@
 							<nav id="dropdown">
 								<ul>
 									<li><a href="index.html">home</a>
-										<ul>
-											<li><a href="index.html">Home Version 1</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/index-2.html">Home Version 2</a></li>
-										</ul>
 									</li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop.html">products</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop-sidebar.html">accesories</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop-list.html">lookbook</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/blog.html">blog</a></li>
-									<li><a href="index.html#">pages</a>
-										<ul>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop.html">Shop</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop-sidebar.html">Shop Sidebar</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/shop-list.html">Shop List</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/single-product.html">Single Product</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/single-product-sidebar.html">Single Product Sidebar</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/cart.html">Shopping Cart</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/wishlist.html">Wishlist</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/checkout.html">Checkout</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/order.html">Order</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/login.html">login / Registration</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/my-account.html">My Account</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/404.html">404</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/blog.html">Blog</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/single-blog.html">Single Blog</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/single-blog-sidebar.html">Single Blog Sidebar</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/about.html">About Us</a></li>
-											<li><a href="https://template.hasthemes.com/hurst-v1/hurst/contact.html">Contact</a></li>
-										</ul>
-									</li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/about.html">about us</a></li>
-									<li><a href="https://template.hasthemes.com/hurst-v1/hurst/contact.html">contact</a></li>
 								</ul>
 							</nav>
 						</div>
