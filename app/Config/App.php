150fc264701a378e26b70dfc092bacbfc8715c06
zoelabbb <zoelabbb@gmail.com>
2023-03-26 20:03:30 +0700

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'https://taupe-gnome-d0e6bf.netlify.app/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
